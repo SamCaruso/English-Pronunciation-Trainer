053064a0514297134063c518328b993a5dce8a6b
Samuele Caruso <carusosamu@hotmail.com>
2025-11-12 22:17:48 +0000

    setUp() in test_phoneme

--- test_phoneme_api.py
+++ test_phoneme_api.py
@@ -20,14 +20,16 @@ logging.getLogger('phoneme_api').disabled = True
 
 class TestPhonemeApi(unittest.TestCase):
     """Test retrieval of the API in 'get_phoneme()' and exception handling"""
-    
+
+    def setUp(self):
+        self.mock_response = Mock()
+        self.mock_response.raise_for_status.return_value = None
+        
     @patch('phoneme_api.requests.get')
     def test_get_phoneme_successful_no_audio(self, mock_get):
-        mock_response = Mock()
-        mock_response.raise_for_status.return_value = None
-        mock_response.json.return_value = [{'phonetics': [{'text': '/ɔː(ɹ)/', 'audio': ''}]}]
+        self.mock_response.json.return_value = [{'phonetics': [{'text': '/ɔː(ɹ)/', 'audio': ''}]}]
         
-        mock_get.return_value = mock_response
+        mock_get.return_value = self.mock_response
         result = get_phoneme('empty_phoneme')
         
         self.assertFalse(result)
@@ -36,15 +38,13 @@ class TestPhonemeApi(unittest.TestCase):
     @patch('phoneme_api.playsound')
     @patch('phoneme_api.requests.get')
     def test_get_phoneme_successful_with_audio(self, mock_get, mock_playsound):
-        mock_response = Mock()
-        mock_response.raise_for_status.return_value = None
-        mock_response.json.return_value = [{'phonetics': [{'text': '/ɔː(ɹ)/', 'audio': 'https://soundaudio.url/good_phoneme.mp3'}]}]
+        self.mock_response.json.return_value = [{'phonetics': [{'text': '/ɔː(ɹ)/', 'audio': 'https://soundaudio.url/good_phoneme.mp3'}]}]
         
-        mock_get.return_value = mock_response        
+        mock_get.return_value = self.mock_response        
         result = get_phoneme('good_phoneme')
         
         self.assertTrue(result)
-        mock_response.raise_for_status.assert_called_once()
+        self.mock_response.raise_for_status.assert_called_once()
         mock_get.assert_called_once_with('https://api.dictionaryapi.dev/api/v2/entries/en/good_phoneme', timeout = 5)
         
         mock_playsound.assert_called_once_with('https://soundaudio.url/good_phoneme.mp3')
@@ -53,11 +53,9 @@ class TestPhonemeApi(unittest.TestCase):
     @patch('phoneme_api.playsound', side_effect = PlaysoundException)
     @patch('phoneme_api.requests.get')
     def test_invalid_audio_input(self, mock_get, mock_playsound):
-        mock_response = Mock()
-        mock_response.raise_for_status.return_value = None
-        mock_response.json.return_value = [{'phonetics': [{'text': '/ɔː(ɹ)/', 'audio': 'https://soundaudio.url/phoneme.mp4'}]}]
+        self.mock_response.json.return_value = [{'phonetics': [{'text': '/ɔː(ɹ)/', 'audio': 'https://soundaudio.url/phoneme.mp4'}]}]
         
-        mock_get.return_value = mock_response
+        mock_get.return_value = self.mock_response
         
         result = get_phoneme('phoneme')
         self.assertFalse(result)
@@ -65,10 +63,7 @@ class TestPhonemeApi(unittest.TestCase):
     
     @patch('phoneme_api.requests.get', side_effect = JSONDecodeError('Value expected', '', 0))
     def test_invalid_json(self, mock_get):
-        mock_response = Mock()
-        mock_response.raise_for_status.return_value = None
-        
-        mock_get.return_value = mock_response
+        mock_get.return_value = self.mock_response
         result = get_phoneme('phoneme')
         
         self.assertFalse(result)
@@ -76,11 +71,9 @@ class TestPhonemeApi(unittest.TestCase):
       
     @patch('phoneme_api.requests.get')
     def test_valid_json_unexpected_format_indexerror(self, mock_get):
-        mock_response = Mock()
-        mock_response.raise_for_status.return_value = None
-        mock_response.json.return_value = {'phonetics': {'text': '/ɔː(ɹ)/', 'audio': 'https://soundaudio.url/good_phoneme.mp3'}}
+        self.mock_response.json.return_value = {'phonetics': {'text': '/ɔː(ɹ)/', 'audio': 'https://soundaudio.url/good_phoneme.mp3'}}
         
-        mock_get.return_value = mock_response
+        mock_get.return_value = self.mock_response
         result = get_phoneme('phoneme')
         
         self.assertFalse(result)
@@ -88,9 +81,7 @@ class TestPhonemeApi(unittest.TestCase):
     
     @patch('phoneme_api.requests.get', side_effect = HTTPError)
     def test_HTTPError_non_existent_phoneme(self, mock_get):
-        mock_response = Mock()
-        
-        mock_get.return_value = mock_response
+        mock_get.return_value = self.mock_response
         result = get_phoneme('non_existent_phoneme')
         
         self.assertFalse(result)
